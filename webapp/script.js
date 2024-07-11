d288912fc6287fcae5bff7541c6cd496372141de
Jawnny <94490893+Jawnny@users.noreply.github.com>
2024-07-11 15:43:05 -0400

    Implemented melee combat style

--- webapp/script.js
+++ webapp/script.js
@@ -1,8 +1,27 @@
+const checkboxes = document.querySelectorAll('.weapon-checkbox');
+let selectedCheckboxes = [];
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            selectedCheckboxes.push(checkbox);
+            if (selectedCheckboxes.length > 2) {
+                const removedCheckbox = selectedCheckboxes.shift();
+                removedCheckbox.checked = false;
+            }
+        } else {
+            selectedCheckboxes = selectedCheckboxes.filter(cb => cb !== checkbox);
+        }
+    });
+});
+
 document.getElementById("simulateButton").addEventListener("click", function(event) {
     event.preventDefault(); // Prevent default button behavior
 
     // Gather input values from the form
     let playerHP = document.getElementById("hpLevel").value;
+    let playerAtk = document.getElementById("attackLevel").value;
+    let playerStr = document.getElementById("strengthLevel").value;
     let playerDefense = document.getElementById("defenseLevel").value;
     let playerRange = document.getElementById("rangeLevel").value;
     let playerMage = document.getElementById("mageLevel").value;
@@ -10,11 +29,17 @@ document.getElementById("simulateButton").addEventListener("click", function(eve
     let armorTier = document.querySelector('input[name="armorChoice"]:checked').value;
     let rangePrayer = document.querySelector('input[name="prayerRChoice"]:checked').value;
     let magePrayer = document.querySelector('input[name="prayerMChoice"]:checked').value;
-    let defensePrayer = document.getElementById("optionalBuff").checked ? document.getElementById("optionalBuff").value : 1.0;
+    let meleePrayer = document.querySelector('input[name="prayerMeChoice"]:checked').value;
+    let steelSkin = document.getElementById("steelSkin").checked;
+    let halberd = document.getElementById("halberd").checked;
+    let bow = document.getElementById("bow").checked;
+    let staff = document.getElementById("staff").checked;
     let iterations = 1000; // Default iterations or you can add another input for iterations
 
     // Print some variables to the console
     console.log("Player HP Level:", playerHP);
+    console.log("Player Atk Level:", playerAtk);
+    console.log("Player Str Level:", playerStr);
     console.log("Player Defense Level:", playerDefense);
     console.log("Player Range Level:", playerRange);
     console.log("Player Mage Level:", playerMage);
@@ -22,18 +47,28 @@ document.getElementById("simulateButton").addEventListener("click", function(eve
     console.log("Total Fish:", totalFish);
     console.log("Range Prayer:", rangePrayer);
     console.log("Mage Prayer:", magePrayer);
-    console.log("Defense Prayer:", defensePrayer);
+    console.log("Melee Prayer: ", meleePrayer);
+    console.log("Defense Prayer:", steelSkin);
+    console.log("Halberd:", halberd);
+    console.log("Bow:", bow);
+    console.log("Staff:", staff);
     console.log("Iterations:", iterations);
 
     // Create a data object to send
     let data = {
         playerHP: parseInt(playerHP),
+        playerStr: parseInt(playerStr),
+        playerAtk: parseInt(playerAtk),
         playerDefense: parseInt(playerDefense),
         playerRange: parseFloat(playerRange),
         playerMage: parseFloat(playerMage),
         magePrayer: parseFloat(magePrayer),
         rangePrayer: parseFloat(rangePrayer),
-        defensePrayer: parseFloat(defensePrayer),
+        meleePrayer: parseFloat(meleePrayer),
+        steelSkin: steelSkin,
+        halberd: halberd,
+        bow: bow,
+        staff: staff,
         armorTier: parseInt(armorTier),
         totalFish: parseInt(totalFish),
         iterations: parseInt(iterations)
